222be29939e7415384c4749e45e21c1605faf439
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-05-21 10:27:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import User from './route/user.route.js';
 
 const app = express();
 app.use(cors({
-    origin:["https://deploy-mern-1whq.vercel.app"],
+    origin:[""],//frontend link
     methods:["POST","GET"],
     credentials:true
 }
