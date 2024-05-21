be22656b7ef98753e8c25bc229f8b394f5d2bf0d
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-05-21 10:16:16 +0530

    Update FreeBook.jsx

--- client/src/Components/FreeBook.jsx
+++ client/src/Components/FreeBook.jsx
@@ -10,7 +10,7 @@ function Freebook() {
   useEffect(() => {
     const getBook = async () => {
       try {
-        const res = await axios.get("http://localhost:4001/book");
+        const res = await axios.get("book-store-api-virid.vercel.app/book");
 
         const data = res.data.filter((data) => data.Category === "Free");
         console.log(data);
