66d1602523b991b368f71156de6c4dc3c7f75cae
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-05-22 10:48:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import path from 'path';
 
 import bookRoute from "./route/book.route.js";
 import User from './route/user.route.js';
