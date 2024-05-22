d1a1753be0126283b9ba7d775fbf1f37c0caf1e8
Rohit Dhuri <128274063+RMDcode@users.noreply.github.com>
2024-05-22 08:55:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -42,6 +42,15 @@ app.get("/", (req, res) => {
 app.use("/book", bookRoute);
 app.use("/user", User);
 
+// Serve static files from the React app
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 app.listen(port, () => {
     console.log(`Server running on localhost:${port}`);
 });
